f8918d42159b21507dce9b3240aa0c1273e1def7
wjh <jiehangwu.cs@gmail.com>
2020-03-21 23:06:43 -0700

    connected socket.io to express

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,18 +1,42 @@
-const express = require("express");
+const app = require("express")();
 const mongoose = require("mongoose");
+const path = require("path");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const key = require("./secret/key");
 
-const app = express();
 const PORT = 3000 || process.env.PORT;
 
 mongoose.connect(key.MONGO_URI, { useNewUrlParser: true });
 
-app.get("/", (req, res) => res.send("Hello"));
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+});
+
+io.on("connection", (socket) => {
+    // upon succesful socket io connection
+    console.log("Welcome to free chat!!!!");
+    
+    socket.on("new message", (data) => {
+        socket.broadcast.emit("new message", {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user left", {
+            username: socket.username,
+        });
+    });
+});
+
+
+
 
 
