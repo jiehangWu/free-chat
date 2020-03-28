8201b18ab5908385db39921097003c2c182debed
wjh <jiehangwu.cs@gmail.com>
2020-03-28 02:11:29 -0700

    fix URL for ajax requests

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const key = require("./secret/key");
-const cors = require("cors");
+// const cors = require("cors");
 require("./model/User");
 require("./model/Message");
 
@@ -17,7 +17,7 @@ const routes = require("./routes/router");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-app.use(cors());
+// app.use(cors());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 10000,
